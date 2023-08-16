87e8c9d14c8dbdd020c27f50c409802963914e19
Rafał Klocek <rafal.klocek.praca@gmail.com>
2023-08-16 21:11:27 +0200

    fixed bug with subpages, and added rwd

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,23 @@
 const apiKey = "42e075d8ddd042fc9c8bfad79f781be9";
 const items = document.querySelectorAll(".main__section-item");
 let currentItemNum = 0;
-items.forEach((item) => {
+
+for(let i = 0; i < 20; i++){
 	fetch(`https://rawg.io/api/games?token&key=${apiKey}`)
 		.then((res) => res.json())
 		.then((data) => {
-			const img = item.firstElementChild;
-			const name = item.children[1];
-			const rating = item.children[2].firstElementChild;
-			const playTime = item.children[2].lastElementChild;
-			const realeseDate = item.lastElementChild;
+			const img = items[i].firstElementChild;
+			const name = items[i].children[1];
+			const rating = items[i].children[2].firstElementChild;
+			const playTime = items[i].children[2].lastElementChild;
+			const realeseDate = items[i].lastElementChild;
 			img.setAttribute("src", data.results[currentItemNum].background_image);
 			name.textContent = data.results[currentItemNum].name;
 			rating.textContent = "Rating " + data.results[currentItemNum].rating;
 			playTime.textContent =
 				"Playtime" + " " + data.results[currentItemNum].playtime + " h";
 			realeseDate.textContent = data.results[currentItemNum].released;
-			item.setAttribute("href", `./games/${data.results[currentItemNum].id}.html`)
+			items[i].setAttribute("href", `./games/${data.results[currentItemNum].id}.html`)
 
 			currentItemNum++;
 
@@ -29,4 +30,4 @@ items.forEach((item) => {
 			}
 		})
 		.catch((error) => console.error("Error:", error));
-});
+}
--- src/js/subsite.js
+++ src/js/subsite.js
@@ -1,5 +1,5 @@
 const url = window.location.pathname;
-const fileID = parseInt(url.slice(7).slice(0, -5));
+const fileID = url.replace(/^\D+/g, "").slice(0, -5);
 const apiKey = "42e075d8ddd042fc9c8bfad79f781be9";
 const img = document.querySelector(".mainsub__section-img");
 const gameName = document.querySelector(".mainsub__section-name");
@@ -7,30 +7,28 @@ const gameRating = document.querySelector(".mainsub__section-rating");
 const gamePlaytime = document.querySelector(".mainsub__section-playtime");
 const screenshotItem = document.querySelectorAll(".mainsub__section-item");
 let currentScreenshot = 1;
-
 fetch(`https://rawg.io/api/games?token&key=${apiKey}`)
 	.then((res) => res.json())
 	.then((data) => {
-		data.results.forEach((game) => {
-			if (game.id == fileID) {
-				img.setAttribute("src", game.background_image);
-				img.setAttribute("alt", game.name);
-				gameName.textContent = game.name;
-				gameRating.textContent = `Rating: ${game.rating}`;
-				gamePlaytime.textContent = `Playtime: ${game.playtime}`;
+		for (let i = 0; i < 20; i++) {
+			if (data.results[i].id == fileID) {
+				img.setAttribute("src", data.results[i].background_image);
+				img.setAttribute("alt", data.results[i].name);
+				gameName.textContent = data.results[i].name;
+				gameRating.textContent = `Rating: ${data.results[i].rating}`;
+				gamePlaytime.textContent = `Playtime: ${data.results[i].playtime}`;
 
 				screenshotItem.forEach((item) => {
 					item.firstElementChild.setAttribute(
 						"src",
-						game.short_screenshots[currentScreenshot].image
+						data.results[i].short_screenshots[currentScreenshot].image
 					);
-					currentScreenshot++;
 					item.addEventListener("click", changePicture);
+					currentScreenshot++;
 				});
 			}
-		});
+		}
 	});
-
 function changePicture() {
 	img.setAttribute("src", this.firstElementChild.getAttribute("src"));
 }
