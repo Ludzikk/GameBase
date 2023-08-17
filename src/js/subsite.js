afd70d39a0a550c10feff5a4902d8702ce724191
Rafał Klocek <rafal.klocek.praca@gmail.com>
2023-08-17 17:02:48 +0200

    added new content to subpages of games

--- src/js/subsite.js
+++ src/js/subsite.js
@@ -6,7 +6,12 @@ const gameName = document.querySelector(".mainsub__section-name");
 const gameRating = document.querySelector(".mainsub__section-rating");
 const gamePlaytime = document.querySelector(".mainsub__section-playtime");
 const screenshotItem = document.querySelectorAll(".mainsub__section-item");
+const platforms = document.querySelector(".mainsub__section-platforms");
+const pcIcon = "fa-solid fa-desktop";
+const psIcon = "fa-brands fa-playstation";
+const xboxIcon = "fa-brands fa-xbox";
 let currentScreenshot = 1;
+
 fetch(`https://rawg.io/api/games?token&key=${apiKey}`)
 	.then((res) => res.json())
 	.then((data) => {
@@ -17,6 +22,28 @@ fetch(`https://rawg.io/api/games?token&key=${apiKey}`)
 				gameName.textContent = data.results[i].name;
 				gameRating.textContent = `Rating: ${data.results[i].rating}`;
 				gamePlaytime.textContent = `Playtime: ${data.results[i].playtime}`;
+
+				data.results[i].parent_platforms.forEach(platform => {
+					const p = document.createElement("p");
+					const i = document.createElement("i");
+					p.setAttribute("class", "mainsub__section-platform");
+					if(platform.platform.id === 1){
+						i.setAttribute("class", pcIcon);
+						i.textContent = " " + platform.platform.name;
+						p.appendChild(i);
+						platforms.appendChild(p);
+					} else if (platform.platform.id === 2){
+						i.setAttribute("class", psIcon);
+						i.textContent = " " + platform.platform.name;
+						p.appendChild(i);
+						platforms.appendChild(p);
+					} else if (platform.platform.id === 3){
+						i.setAttribute("class", xboxIcon);
+						i.textContent = " " + platform.platform.name;
+						p.appendChild(i);
+						platforms.appendChild(p);
+					}
+				})
 
 				screenshotItem.forEach((item) => {
 					item.firstElementChild.setAttribute(
